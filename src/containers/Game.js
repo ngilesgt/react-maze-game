430b793bf3a832a2b1bb57df3fdf19965c7c01e0
Noah Giles <ng250198@ncr.com>
2019-12-29 20:35:07 -0500

    added components, styled components, css animations and more reducers

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,11 +1,21 @@
 import { connect } from "react-redux";
-
 import App from "../App";
+import { moveObjects, startGame } from "../actions/index";
 
 const mapStateToProps = state => ({
-  message: state.message
+  angle: state.angle,
+  gameState: state.gameState
 });
 
-const Game = connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  moveObjects: mousePosition => {
+    dispatch(moveObjects(mousePosition));
+  },
+  startGame: () => {
+    dispatch(startGame());
+  }
+});
+
+const Game = connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default Game;
