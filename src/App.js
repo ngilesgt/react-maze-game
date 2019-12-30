02841c4f8e891179d10b4ab71253e24ea4dc16e6
Noah Giles <ng250198@ncr.com>
2019-12-30 15:45:45 -0500

    added collision, leaderboard, socket.io, server, and score

--- src/App.js
+++ src/App.js
@@ -2,10 +2,57 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { getCanvasPosition } from "./utils/formulas";
 import Canvas from "./components/Canvas";
+import * as Auth0 from "auth0-web";
+import io from "socket.io-client";
+
+Auth0.configure({
+  domain: "ngiles.auth0.com",
+  clientID: "aynRIcpHjsYzAmjanCJRKyZLuT8dn0cZ",
+  redirectUri: "http://localhost:3000/",
+  responseType: "token id_token",
+  scope: "openid profile manage:points",
+  audience: "https://aliens-go-home.digituz.com.br"
+});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.shoot = this.shoot.bind(this);
+    this.socket = null;
+    this.currentPlayer = null;
+  }
+
   componentDidMount() {
     const self = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe(auth => {
+      if (!auth) return;
+
+      self.playerProfile = Auth0.getProfile();
+      self.currentPlayer = {
+        id: self.playerProfile.sub,
+        maxScore: 0,
+        name: self.playerProfile.name,
+        picture: self.playerProfile.picture
+      };
+
+      this.props.loggedIn(self.currentPlayer);
+
+      self.socket = io("http://localhost:3001", {
+        query: `token=${Auth0.getAccessToken()}`
+      });
+
+      self.socket.on("players", players => {
+        this.props.leaderboardLoaded(players);
+        players.forEach(player => {
+          if (player.id === self.currentPlayer.id) {
+            self.currentPlayer.maxScore = player.maxScore;
+          }
+        });
+      });
+    });
 
     setInterval(() => {
       self.props.moveObjects(self.canvasMousePosition);
@@ -19,17 +66,35 @@ class App extends Component {
     window.onresize();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.gameState.started && this.props.gameState.started) {
+      if (this.currentPlayer.maxScore < this.props.gameState.kills) {
+        this.socket.emit("new-max-score", {
+          ...this.currentPlayer,
+          maxScore: this.props.gameState.kills
+        });
+      }
+    }
+  }
+
   trackMouse(event) {
     this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  shoot() {
+    this.props.shoot(this.canvasMousePosition);
   }
 
   render() {
     return (
       <Canvas
         angle={this.props.angle}
+        currentPlayer={this.props.currentPlayer}
         gameState={this.props.gameState}
+        players={this.props.players}
         startGame={this.props.startGame}
         trackMouse={event => this.trackMouse(event)}
+        shoot={this.shoot}
       />
     );
   }
@@ -52,7 +117,29 @@ App.propTypes = {
     ).isRequired
   }).isRequired,
   moveObjects: PropTypes.func.isRequired,
-  startGame: PropTypes.func.isRequired
+  startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired
+  }),
+  leaderboardLoaded: PropTypes.func.isRequired,
+  loggedIn: PropTypes.func.isRequired,
+  players: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      maxScore: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired
+    })
+  ),
+  shoot: PropTypes.func.isRequired
+};
+
+App.defaultProps = {
+  currentPlayer: null,
+  players: null
 };
 
 export default App;
